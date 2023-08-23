2ea464a80defdda36c733ada1dbb89dc478699bf
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2023-08-23 13:35:02 +0300

    [general] feat: add missing copyright

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 export { ERC20ABI, ERC721ABI, ERC1155ABI } from './ABI';
