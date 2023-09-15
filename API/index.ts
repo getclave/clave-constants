978e4fd756be9071e9dc33ce009a65909c781c1e
zetsuboii <hmzkrb@gmail.com>
2023-09-15 18:19:02 +0400

    [notif-con] feat: move to clave-controller

--- API/index.ts
+++ API/index.ts
@@ -3,5 +3,4 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-export { ERC20ABI, ERC721ABI, ERC1155ABI } from './ABI';
-export { API_DEV_BASE_URL } from './API';
+export const API_DEV_BASE_URL = 'https://dev-api.clave.team';
