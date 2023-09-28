88720958851b42f2e77d587df5910ac25db70fbe
aalimsahin <alimsahin0007@gmail.com>
2023-09-28 22:43:17 +0300

    [clave-core] feat: updated transfer function

--- address/index.ts
+++ address/index.ts
@@ -3,6 +3,7 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-export const CONSTANT_ADDRESSES = {
+export const CONSTANT_ADDRESSES: Record<string, string> = {
     MULTICALL3: '0xF9cda624FBC7e059355ce98a31693d299FACd963',
+    VALIDATOR_ADDRESS: '0xF9cda624FBC7e059355ce98a31693d299FACd963',
 };
