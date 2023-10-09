d3c9bbf68f743e836be23af77712cb27d5e75fd7
asgarovf <fasgarov2002@gmail.com>
2023-10-09 04:48:09 +0300

    [zksync-mobile] init: paymaster attachment

--- abi/index.ts
+++ abi/index.ts
@@ -7,3 +7,4 @@ export { MULTICALL3_ABI } from './MULTICALL3ABI';
 export { ERC20ABI } from './ERC20ABI';
 export { ERC721ABI } from './ERC721ABI';
 export { ERC1155ABI } from './ERC1155ABI';
+export { PAYMASTERABI } from './PAYMASTERABI';
